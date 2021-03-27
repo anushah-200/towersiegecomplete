17cbf6a37dc05e69f840ea8c54435e5a5d41c560
anushah-200 <77103799+anushah-200@users.noreply.github.com>
2021-03-27 09:13:45 +0530

    Update box.js

--- box.js
+++ box.js
@@ -1,44 +1,44 @@
-class Box{
-    constructor(x, y, width, height) {
-        var options = {
-            'restitution':0.8,
-            'friction':1.0,
-            'density':1.0
-            
-        }
-        this.body = Bodies.rectangle(x, y, width, height, options);
-        this.width = width;
-        this.height = height;
-        this.image = loadImage("aredbox.png");
-        World.add(world, this.body);
-        this.visibility=255
-      }
-      display(){
-        
-        push();
-        translate(this.body.position.x, this.body.position.y);
-        
-        imageMode(CENTER);
-        if(this.body.speed<3)
-        image(this.image, 0, 0, this.width, this.height);
-        else{
-        World.remove(world,this.body)
-        this.visibility=this.visibility-5
-         tint(255,this.visibility)
-         image(this.image, 0, 0, this.width, this.height);}
-        
-       
-        
-        
 
+         class Box{
+          constructor(x, y, width, height, angle) {
+              var options = {
+                restitution : 0.7,
+                friction : 0.6,
+                isStatic : false,
+                
+              }
+              this.body = Bodies.rectangle(x, y, width, height, options);
+              this.width = width;
+              this.height = height;
+              this.Visibility =255;
+              World.add(world, this.body);
+              
+            }
+            display(){
+              
+              
+              var angle = this.body.angle;
+              if(this.body.speed<2.5){
+              push();
+              translate(this.body.position.x, this.body.position.y);
+              rotate(angle);
+              rectMode(CENTER);
+              rect(0, 0, this.width, this.height);
+              pop();
+              }else {
+                World.remove(world, this.body)
+                push();
+                this.Visibility = this.Visibility -10;
+                tint(255, this.Visibility)
+                
+                pop();
+              }
+            }
+            score(){
       
-        
-         pop();
-         }}
-         score()
-         {
-          push()
-         if(this.Visiblity<0 && this.Visiblity>=-1005){
-         score++
-         pop()
-         }}
+              if(this.Visibility<0 && this.Visibility > -105){
+                score++;
+              }
+      
+            }
+          }
